57b968c99bd7f0f0dc17e079d70700fe73653474
BRIAN MAHER <bmaher22@mac.comclear>
2023-09-26 11:39:26 -0400

    initial

--- index.js
+++ index.js
@@ -1,56 +1,88 @@
 
-
-var students = ['brandon', 'lorchesca', 'kevin', 'ayman', 'pablo'];
-var count = 30;
-function getRandomStudent() {
-    var studentIndex = Math.floor(Math.random() * students.length);
-    var student = students[studentIndex];
-
-    return student;
-}
-
-function cycle() {
-    var interval = setInterval(function () {
-        var student = getRandomStudent();
-       
-        console.clear();
-        console.log(student);
-
-        count --;
-        if (!count) {
-            console.clear();
-            clearInterval(interval);
-            console.log(student, 'has been chosen!');
-        }
-        }, 70);
-}
-
-
-cycle();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var a = 5;
-// var b = 10;
-
-// function test () {
-//     console.log('testing!');
+var data = {
+    name: 'JD',
+    age: 43,
+    printName: () => {
+console.log(this.name);
+    },
+// printNameStandard: function() {
+    
 // }
 
 
-// console.log(a + b);
 
-// test ();
+};
+
+data.printName();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var students = ['brandon', 'lorchesca', 'kevin', 'ayman', 'pablo'];
+// var count = 30;
+// function getRandomStudent() {
+//     var studentIndex = Math.floor(Math.random() * students.length);
+//     var student = students[studentIndex];
+
+//     return student;
+// }
+
+// function cycle() {
+//     var interval = setInterval(function () {
+//         var student = getRandomStudent();
+       
+//         console.clear();
+//         console.log(student);
+
+//         count --;
+//         if (!count) {
+//             console.clear();
+//             clearInterval(interval);
+//             console.log(student, 'has been chosen!');
+//         }
+//         }, 70);
+// }
+
+
+// cycle();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // var a = 5;
+// // var b = 10;
+
+// // function test () {
+// //     console.log('testing!');
+// // }
+
+
+// // console.log(a + b);
+
+// // test ();
